4b7c85e8d2400de5842b97b79874f08cdef7ac79
Gustavo <boteongustavo@gmail.com>
2021-07-14 14:58:42 -0300

    add user

--- createRepr.py
+++ createRepr.py
@@ -1,13 +1,11 @@
-from typing import Sized
-from numpy import ERR_CALL
 import pandas as pd
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 
 print("Bot para cadastro de Representantes")
+user = str(input("Digite seu user:\n"))
 planilha = str(input("Insira o caminho da planilha:\n"))
 df = pd.read_excel(planilha)
 try:
@@ -22,9 +20,10 @@ except AttributeError:
         coluna = "NOME"
 
 val = False
-tempo = 12 + 6 + (7*nomes.size)
+tempo = 18 + 7 + (7*nomes.size)
+planilha = planilha.split("\\")
 
-print("\nEsse bot fara o cadastro dos colaboradores na planilha *" + planilha + "* possicionados na coluna *" + coluna + "*,\natualmente existem " + str(nomes.size) + " colaboradores a serem adicionados e a operação ira demorar aproximadamente "+ str(tempo) + " segundos\n")
+print("\nEsse bot fara o cadastro dos colaboradores na planilha *" + planilha[len(planilha)-1] + "* possicionados na coluna *" + coluna + "*,\natualmente existem " + str(nomes.size) + " colaboradores a serem adicionados e a operação ira demorar aproximadamente "+ str(tempo) + " segundos\n")
 menu = int(input("Qual será o representante dos colaboradores da planinha\n1.Eliana Subires dos Santos\n2.Jennifer Cruz Asunção\n3.Juliana Cecilia Saiki Dutra\n4.Daniele Gomes Souza\n5.Priscilla Tavares\n6.Bruna Correia de Souza\n7.Gilglezia Maria  de Souza Oliveira\nDigite o Numero: "))
 if menu == 1:
     nomeRep = "eliana.subires"
@@ -42,10 +41,11 @@ elif menu == 7:
     nomeRep = "gilglezia.oliveira"
 
 cont = 0
+execTime = time.perf_counter()
 
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument(r"user-data-dir=C:\Users\gustavo.boteon\AppData\Local\Google\Chrome\User Data\Default")
+chrome_options.add_argument(r"user-data-dir=C:\\Users\\" + user + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
 driver = webdriver.Chrome(executable_path=r'./chromedriver.exe', options=chrome_options)
 #driver.get("https://ensp-365-axprod.operations.dynamics.com/?cmp=001&mi=DefaultDashboard")
 driver.get("https://enesa-uat.sandbox.operations.dynamics.com/?cmp=001&mi=DefaultDashboard")
@@ -105,6 +105,8 @@ finally:
         driver.find_element_by_xpath('//*[@id="trvappremplsub_2_SystemDefinedSaveButton"]').click()
         time.sleep(1)
         cont += 1
-print("processo finalizado com sucesso!")
+        
+endTime = time.perf_counter() - execTime
+print("Processo finalizado com sucesso!\na Executção do progama demorou " + str(endTime) + " segundos")
 val = input("Pressione ENTER para finalizar o progama")
 driver.quit()
--- testecsv.py
+++ testecsv.py
@@ -1,5 +1,16 @@
+import time
+from datetime import datetime
 import pandas as pd
 
-df = pd.read_excel('teste func.xlsx')
+
+planilha = str(input("Insira o caminho da planilha:\n"))
+df = pd.read_excel(planilha)
 nomes = df.nome
 print(nomes.size)
+planilha = planilha.split("\\")
+print(planilha[len(planilha)-1])
+
+#data_e_hora_atuais = datetime.now()
+#data_e_hora_em_texto = data_e_hora_atuais.strftime("%d/%m/%Y")
+
+#print(data_e_hora_em_texto)
